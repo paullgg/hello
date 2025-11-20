2c6445f9a4ee940b2fb5e5a456e03cd8c8578482
Praanjal <praanjal.pakhrin@gmail.com>
2025-11-20 10:32:27 +0545

    why

---
+++ h.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class h here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class h
+{
+    public static void main(String[] args){
+        
+    System.out.println("hello boss");
+    }
+}
